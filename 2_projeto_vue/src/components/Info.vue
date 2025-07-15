54e65553c874a055361af67dae80cd6be3946724
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-15 15:46:10 -0300

    Direticas

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -2,7 +2,9 @@
     
     <div>
 
-        <p>Estou estudando no momento.</p>
+        <p v-if='esta_estudando'>Estou estudando no momento.</p>
+
+        <p v-else>Estou em busca de novas oportunidades!</p>
 
         <p>Utilizo as seguintes tecnologias:</p>
 
@@ -12,12 +14,23 @@
             <li>JavaScript</li>
         </ul>
 
+        <p v-if='4 > 2'>Testando</p>
+
+        <p v-show='mostrar_email'>Mande uma mensagem para: {{ email }}</p>
+
     </div>
 
 </template>
 
 <script>
     export default {
-        name: 'Info'
+        name: 'Info',
+        data() {
+            return {
+                esta_estudando: false,
+                mostrar_email: true,
+                email: 'lucas@gmail.com'
+            }
+        }
     }
 </script>
