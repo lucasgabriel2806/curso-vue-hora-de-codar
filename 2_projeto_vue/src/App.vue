8943eb6d4ef65b9ca994db6b84760f225f827f17
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-15 15:29:16 -0300

    Hierarquia de componentes

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -8,22 +8,26 @@
 
     <LifeCycle />
 
+    <Pessoa />
+
   </div>
 
 </template>
 
 <script>
-import LifeCycleVue from './components/LifeCycle.vue';
 
   import PrimeiroComponente from './components/PrimeiroComponente.vue';
 
   import LifeCycle from './components/LifeCycle.vue';
 
+  import Pessoa from './components/Pessoa.vue';
+
   export default {
     name: 'App',
     components: {
       PrimeiroComponente,
-      LifeCycle
+      LifeCycle,
+      Pessoa
     },
     data() {
       return {
---
+++ 2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,23 @@
+<template>
+    
+    <div>
+
+        <p>Estou estudando no momento.</p>
+
+        <p>Utilizo as seguintes tecnologias:</p>
+
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+        </ul>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'Info'
+    }
+</script>
