76510fe380893699e367c63104b637a65e53d183
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-16 15:12:16 -0300

    Atributos dinâmicos

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -18,18 +18,29 @@
 
         <p v-show='mostrar_email'>Mande uma mensagem para: {{ email }}</p>
 
+        <p>Para acessar meu portfólio <a v-bind:href="meu_link">basta clicar aqui</a></p>
+
+        <Picture />
+
     </div>
 
 </template>
 
 <script>
+
+    import Picture from './Picture.vue';
+
     export default {
         name: 'Info',
+        components: {
+            Picture
+        },
         data() {
             return {
                 esta_estudando: false,
                 mostrar_email: true,
-                email: 'lucas@gmail.com'
+                email: 'lucas@gmail.com',
+                meu_link: 'https://google.com'
             }
         }
     }
---
+++ 2_projeto_vue/src/components/Picture.vue
@@ -0,0 +1,17 @@
+<template>
+    
+    <img :src="avatar" :alt="descricao">
+
+</template>
+
+<script>
+    export default {
+        name: 'Picure',
+        data() {
+            return {
+                avatar: '/img/avatar.png',
+                descricao: 'Lucas Gabriel'
+            }
+        }
+    }
+</script>
